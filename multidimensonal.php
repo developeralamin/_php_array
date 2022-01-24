04541164db4699267dc9fbcd2d4447d1129ef154
developeralamin <ribinaofficial018@gmail.com>
2022-01-24 20:22:33 +0600

    add php array

---
+++ multidimensonal.php
@@ -0,0 +1,1364 @@
+
+<?php
+
+  echo "<h1>Multidimensional Array</h1>";
+ 
+
+echo '<pre>';
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+    echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+
+  $multiArray = [
+    				[
+                20,3,4,
+    				], 
+              [
+                'second_array',5,7
+              ],5,
+
+    			      [22,4,6,
+    		  				[
+  		 				'hello',4,5
+  		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+    echo '<br>';
+  echo $multiArray[0][0];
+ echo '<br>';
+   echo $multiArray[1][0];
+    echo '<br>';
+
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+   print_r($multiArray[3][3][0]);
+   echo '<br>';
+  print_r($multiArray[0][0]);
+    echo '<br>';
+
+   //2d array
+   $arry = array(
+             array('camel_case','snake_case',3),
+         array(32,4,6)
+       ,3,5);
+   
+  print_r($arry[0][1]);
+  echo '</br>';
+  print_r( $arry[1]);
+
+
+
+//3d array
+   $arry = array(
+             array(
+          array('kodu','llou','lulu'),
+            2,3),
+         array('hurray','ha ha','moja')
+         ,3,5);  
+
+ print_r($arry[0][0][1]);
+  echo '</br>';
+ print_r($arry[1][0]);
+
+ echo "<br>";
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+
+  $multiArray = [
+    				[
+                    20,3,4,
+    				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+    echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+    
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+ 
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+  
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+ 
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+    echo '<br>';
+     echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+    echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+    echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+    echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+    echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+    echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+    echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+    echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+    echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+    echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+    echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+    echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+    echo '<br>';
+     echo '<br>';
+  echo $multiArray[0][0];
+   echo '<br>';
+    echo '<br>';
+     echo '<br>';
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+    echo '<br>';
+     echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+    echo '<br>';
+     echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+    echo '<br>';
+     echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+    echo '<br>';
+     echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+    echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
+  $multiArray = [
+  				[
+                  20,3,4,
+  				],4,5,
+
+  			      [22,4,6,
+		  				[
+		 				'hello',4,5
+		  				]
+  			]
+  ];
+
+  echo $multiArray[3][3][0];
+   echo '<br>';
+  echo $multiArray[0][0];
+    echo '<br>';
+
+
