ecc36fa5787019c568c09b61b6f81b1119342c14
developeralamin <ribinaofficial018@gmail.com>
2022-01-27 16:17:58 +0600

    Try for maintain sequences

--- array_deep_discuss.php
+++ array_deep_discuss.php
@@ -1,266 +1,48 @@
 <?php  
-
-  echo "<h1>Index Arrray</h1>";
-  echo '<br>';
-
-echo '<pre>';
- $variable = array(9,0,45,3);
-
-   echo $variable[0];
-
-   echo "</br>";
-
- $_var  = array(1,2,45,7,7,8);
-
-   echo $_var[4];
-    echo "</br>";
-
- $arr10230 = array(1,2,45,7,7,8);
-
-   echo $arr10230[1];
-    echo "</br>";
-
- $arr100 = array(1,2,45,45,7,8);
-
-   echo $arr100[3];
-    echo "</br>";
-
- $arr1000 = array(1,2,45,4,7,8);
-
-   echo $arr1000[4];
-    echo "</br>";
-
- $arr10000 = array(1,2,45,7,7,8);
-
-   echo $arr10000[3];
-    echo "</br>";
-
- $arr1100 = array(1,2,45,1,7,8);
-
-   echo $arr1100[0];
-    echo "</br>";
-
- $arr22100 = array(1,2,45,1,7,8);
-
-   echo $arr22100[5];
-    echo "</br>";
-
- $arr656100 = array(1,2,45,7,7,8);
-
-   echo $arr656100[5];
-    echo "</br>";
-
- $arrf100 = array(1,2,45,7,7,8);
-
-   echo $arrf100[5];
-    echo "</br>";
-
- $arrrt100 = array(178,9,2,45,7,7,8);
-
-   echo $arrrt100[5];
-    echo "</br>";
-
- $arryu100 = array(1,2,45,8,9,97,7,8);
-
-   echo $arryu100[2];
-    echo "</br>";
-
- $arrfgh100 = array(1,2,45,74,5,6,7,8);
-
-   echo $arrfgh100[2];
-
- echo "</br>";
- $asdfrr100 = array(1,2,45,7,7,8);
- 
-     echo $asdfrr100[2];
-      echo "</br>";
-
- $arsdr100 = array(1,2,45,7,7,8);
- 
-     echo $arsdr100[3];
-
-      echo "</br>";
-
- $arsdr100 = array(1,2,45,7,7,8);
- 
-     echo $arsdr100[3];
-
-      echo "</br>";
-
- $arrs100 = array(1,2,45,7,7,8);
- 
-     echo $arrs100[3];
-
-      echo "</br>";
-
- $arrfs100 = array(1,2,45,7,7,8);
- 
-     echo $arrfs100[3];
-
-      echo "</br>";
-
- $arrd100 = array(35,78,1,2,45,7,7,8);
- 
-     echo $arrd100[3];
-
-      echo "</br>";
-
- $arr10fg0 = array(7,1,2,45,7,7,8);
- 
-     echo $arr10fg0[4];
-
-      echo "</br>";
-
- $arfgr100 = array(56,1,2,2,7,7,8);
- 
-     echo $arfgr100[4];
-
-      echo "</br>";
-
- $arhr100 = array(1,2,45,0,435,8);
- 
-     echo $arhr100[4];
-
-      echo "</br>";
-
- $arhghr100 = array(1,2,45,7,7,8);
- 
-     echo $arhghr100[4];
-
-      echo "</br>";
-
- $arrgr100 = array(1,2,45,7,7,8);
- 
-     echo $arrgr100[3];
-
-      echo "</br>";
-
- $arrrt100 = array(1,2,45,7,7,8);
- 
-     echo $arrrt100[3];
-
-      echo "</br>";
-
- $arfgr100 = array(1,2,45,7,7,8);
- 
-     echo $arfgr100[3];
-
-      echo "</br>";
-
- $arrfg100 = array(1,2,45,7,7,8);
- 
-     echo $arrfg100[3];
-
-      echo "</br>";
-
- $argr100 = array(1,2,45,7,7,8);
- 
-     echo $argr100[4];
-
-      echo "</br>";
-
- $arfgr100 = array(1,2,45,7,7,8);
- 
-     echo $arfgr100[4];
-
-      echo "</br>";
-
- $ar56r100 = array(1,2,45,7,7,8);
- 
-     echo $ar56r100[4];
-
-      echo "</br>";
-
- $arfsgr100 = array(0,1,2,45,7,7,8);
- 
-     echo $arfsgr100[4];
-
-      echo "</br>";
-
- $test = array(1,78,2,45,7,7,8);
- 
-     echo $test[4];
-
-      echo "</br>";
-
-
- $text21 = array(0,56,1,2,45,7,7,8);
-   
-   echo $text21[1];
-
-    echo "</br>";
-
- $test23 = array(2,45,7,7,8,67,7);
-   
-   echo $test23[2];
-
-    echo "</br>";
-
- $test56 = array(45,1,2,67,45,7,7,8);
-   
-   echo $test56[2];
-
-    echo "</br>";
-
- $var567 = array(12,1,4,6,2,45,7,7,8);
-   
-   echo $var567[2];
-
- echo "</br>";
-
- $arrdfd100 = array(1,2,45,7,7,8);
- 
- $pro = [1,3,4,5,6];
- echo $pro[4];
- 
-  echo "</br>";
- $arr44     = [1,2,45,7,7,8];
-  echo $arr44[2];
-
- echo "</br>";
- $arr56     = [1,2,45,7,7,8];
-  echo $arr56[1];
-
- echo "</br>";
- $arr78     = [6,9,0,9,1,2,4];
-  echo $arr78[1];
-
-   echo "</br>";
-
- $arr2      = [3,9,01,2,45,7,7,8];
-  echo $arr2[6];
-   echo "</br>";
-
- $arr1      = [17,88,7,2,45,7,7,8];
-  echo $arr1[4];
-   echo "</br>";
-
- $arry 		= [4543,4556,1,2,45,7,7,8];
-  echo $arry[3];
-   echo "</br>";
-
- $arry100   = [1,2,454,4656,45,7,7,8];
-  echo $arry100[4];
-   echo "</br>";
- 
-
- $test4545   = [16,26,45,56,7,8];
-  echo $test4545[5];
-
- echo "</br>";
-
-   $testY   = [6,45,7,7,8];
-  echo $testY[3];
-
- echo "</br>";
- 
-  $testcase   = [454,565,4656,45,78];
-  echo $testcase[4];
-
-   echo "</br>";
+  // ========================================================//
+  // ==============Indexed Array Practise==================//
+  // ======================================================//
   
-   $dev   = [454,4656,45,7,7,8];
-  echo $dev[2]; 
+  
+  echo "<h1>Indexed Array</h1>";
+  echo '<br>';
+  
+  echo '<pre>';
+  
+ $ProgrammerList = array('computer specialist','Engineer','Guru','Hacker');
+ echo 'ProgrammerList is = '. $ProgrammerList[0];
+ echo "</br>";
+
+ $WhiteBirdName  = array('Diablo ','Harvey Beaks','Dab','Ace','Owl');
+ echo 'WhiteBirdName is = ' .$WhiteBirdName[4];
+ echo "</br>";
+
+ $BestBusName = array('VOLVO ','Hanif','Shamoli','GreenLine','Desh Travel');
+ echo 'Best Bus Name is = ' .$BestBusName[2];
+ echo "</br>";
+
+ $BestTrainName = array('Banalata Express','Panchagarh Express','Ekota Express');
+ echo 'Best Train Name is = ' . $BestTrainName[1];
+ echo "</br>";
+
+ $TopProgramming = array('Python','JavaSript','PHP','Go','Scala');
+ echo 'TopProgramming for Software Development = ' .$TopProgramming[2];
+ echo "</br>";
+
+ $BestTextEditor = array('VS Code','Sublime Text','Notepadd++','phpstorm');
+ echo 'Best Text Editor is = ' .$BestTextEditor[1];
+ echo "</br>";
+
+
+
+  // ========================================================//
+  // ==============Indexed Array Practise==================//
+  // ======================================================//
+
+
+
+
+ 
 
 
 
--- associative_array.php
+++ associative_array.php
@@ -1,249 +1,40 @@
 <?php 
-  echo "<h1>Associative Arrray</h1>";
+  // ========================================================//
+  // ==============Associative Array Practise==============//
+  // ======================================================//
+
+
+  echo "<h1>Associative Array</h1>";
   echo '<br>';
   
   echo '<pre>';
+  
+  $TopMosque = array('MosqueName'=>'Masjid al-Haram','Capacity' =>4,000,000,'city'=>'Mecca');
+  echo 'TopMosque is name = ' .$TopMosque['MosqueName'];
+  echo '<br>';
 
-   $info = array('name'=>'alamin','age' =>34,'city'=>'DOHS');
-   echo $info['name'];
-echo '<br>';
+  $NorthTopDistrict = array('DistrictName'=>'Rangpur','Upazilla' =>58,'Union'=>536);
+  echo $NorthTopDistrict['Upazilla'];
+  echo '<br>';
 
-     $info = array('name'=>'alamin','age' =>34,'city'=>'DOHS');
-   echo $info['age'];
    
-   echo '<br>';
+  $TopFood = array('FoodName'=>'Biriyani','Quantity' =>'4 Plate','Place'=>'Mirpur-12');
+  echo 'TopFood Name is = ' .$TopFood['FoodName'];
+  echo '<br>';
    
-      $info = array('name'=>'alamin','age' =>34,'city'=>'DOHS');
-   echo $info['age'];
-   echo '<br>';
-      $info = array('name'=>'alamin','age' =>34,'city'=>'DOHS');
-   echo $info['name'];
-   echo '<br>';
+  $BlackBirdName = array('BirdName'=>'Crow','leg' =>2,'color'=>'Black');
+  echo 'Black Bird Name is = ' .$BlackBirdName['BirdName'];
+  
+  // ========================================================//
+  // ==============Associative Array Practise==============//
+  // ======================================================//
+  
+  
+ 
+
    
-      $info = array('name'=>'alamin','age' =>34,'city'=>'DOHS');
-   echo $info['city'];
-   echo '<br>';
-      $info = array('name'=>'alamin','age' =>34,'city'=>'DOHS');
-   echo $info['name'];
-   echo '<br>';
-      $info = array('name'=>'alamin','age' =>34,'city'=>'DOHS');
-   echo $info['age'];
-   echo '<br>';
-      $info = array('name'=>'alamin','age' =>34,'city'=>'DOHS');
-   echo $info['name'];
-   echo '<br>';
-      $info = array('name'=>'alamin','age' =>34,'city'=>'DOHS');
-   echo $info['age'];
-   echo '<br>';
-   
-   $info = array('name'=>'alamin','age' =>34,'city'=>'rangpur');
-   echo $info['city'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin','age' =>34,'city'=>'rangpur');
-   echo $info['city'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin','age' =>34,'city'=>'rangpur');
-   echo $info['city'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin','age' =>34,'city'=>'rangpur');
-   echo $info['city'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin','age' =>34,'city'=>'rangpur');
-   echo $info['city'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin','age' =>34,'city'=>'DOHS');
-   echo $info['city'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin','age' =>34,'city'=>'DOHS');
-   echo $info['city'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin','age' =>34,'city'=>'DOHS');
-   echo $info['city'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin','age' =>34,'city'=>'DOHS');
-   echo $info['city'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin','age' =>34,'city'=>'mirpur-12');
-   echo $info['city'];
-   echo '<br>';
-   
-   echo $info['city'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin','age' =>34,'city'=>'DOHS');
-   echo $info['city'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin','age' =>34,'knowledge'=>'programming', 'city'=>'DOHS');
-   echo $info['city'];
-   echo '<br>';
-      $info = array('name'=>'alamin','age' =>34,'knowledge'=>'programming', 'city'=>'DOHS');
-   echo $info['knowledge'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin','age' =>34,'knowledge'=>'coding', 'city'=>'DOHS');
-   echo $info['knowledge'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin','age' =>34,'knowledge'=>'provlem solve', 'city'=>'DOHS');
-   echo $info['city'];
-   echo '<br>';
-      $info = array('name'=>'alamin','age' =>34,'knowledge'=>'design', 'city'=>'DOHS');
-   echo $info['knowledge'];
-   echo '<br>';
-      $info = array('name'=>'alamin','age' =>34,'knowledge'=>'devlop', 'city'=>'DOHS');
-   echo $info['knowledge'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin','age' =>34,'knowledge'=>'devops', 'city'=>'DOHS');
-   echo $info['knowledge'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin','age' =>34,'city'=>'DOHS');
-   echo $info['city'];
-   echo '<br>';
-      $info = array('name'=>'alamin','age' =>34,'city'=>'DOHS');
-   echo $info['city'];
-   echo '<br>';
-      $info = array('name'=>'alamin','age' =>34,'city'=>'DOHS');
-   echo $info['city'];
-   echo '<br>';
-      $info = array('name'=>'alamin','age' =>34,'city'=>'DOHS');
-   echo $info['city'];
-   echo '<br>';
-   
-   
-   
-      $info = array('name'=>'alamin', 'gender' =>'male', 'age' =>34,'city'=>'DOHS');
-   echo $info['gender'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin', 'gender' =>'male' ,'age' =>34,'city'=>'DOHS');
-   echo $info['gender'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin', 'gender' =>'male', 'age' =>34,'city'=>'DOHS');
-   echo $info['gender'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin', 'gender' =>'female' ,'age' =>34,'city'=>'DOHS');
-   echo $info['gender'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin', 'gender' =>'female', 'age' =>34,'city'=>'DOHS');
-   echo $info['gender'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin', 'gender' =>'female', 'age' =>34,'city'=>'DOHS');
-   echo $info['gender'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin','age' =>34,'city'=>'DOHS');
-   echo $info['city'];
-   echo '<br>';
-   
-   
-      $info = array('name'=>'alamin','age' =>34,'city'=>'mirpur-10');
-   echo $info['city'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin','age' =>34,'city'=>'mirpur-10');
-   echo $info['city'];
-   echo '<br>';
-   
-   
-      $info = array('name'=>'alamin','age' =>34,'city'=>'mirpur-10');
-   echo $info['city'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin','age' =>34,'city'=>'mirpur-10');
-   echo $info['city'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin','age' =>34,'city'=>'mirpur-10');
-   echo $info['city'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin','age' =>34,'city'=>'mirpur-10');
-   echo $info['city'];
-   echo '<br>';
-      $info = array('name'=>'alamin','age' =>34,'city'=>'mirpur-10');
-   echo $info['city'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin','age' =>34,'city'=>'DOHS');
-   echo $info['city'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin','age' =>34,'city'=>'DOHS');
-   echo $info['city'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin','age' =>34,'city'=>'DOHS');
-   echo $info['city'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin','age' =>34,'city'=>'road-12');
-   echo $info['city'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin','age' =>34,'city'=>'road-12');
-   echo $info['city'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin','age' =>34,'city'=>'road-12');
-   echo $info['city'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin','age' =>34,'city'=>'road-12');
-   echo $info['city'];
-   echo '<br>';
-      $info = array('name'=>'alamin','age' =>34,'city'=>'road-12');
-   echo $info['city'];
-   echo '<br>';
-   
-   
-      $info = array('name'=>'alamin','age' =>34,'city'=>'DOHS');
-   echo $info['city'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin','age' =>34,'city'=>'DOHS');
-   echo $info['city'];
-   echo '<br>';
+  
    
 
    
-   
-      $info = array('name'=>'alamin','age' =>34, 'status'=>'high' ,'city'=>'DOHS');
-   echo $info['status'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin','age' =>34, 'status'=>'high' ,'city'=>'DOHS');
-   echo $info['status'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin','age' =>34, 'status'=>'high', 'city'=>'DOHS');
-   echo $info['status'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin','age' =>34, 'status'=>'low', 'city'=>'DOHS');
-   echo $info['city'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin','age' =>34, 'status'=>'midium', 'city'=>'DOHS');
-   echo $info['status'];
-   echo '<br>';
-   
-      $info = array('name'=>'alamin','age' =>34, 'status'=>'qualityfull', 'city'=>'DOHS');
-   echo $info['status'];
-   echo '<br>';
-   
+    
